49cf800e4452dfea395a29636598114ec50b1acd
Šarūnas Likas <sarunas.likas@vlk.lt>
2026-09-25 13:38:29 +0300

    pakeitimai layout

--- tables.js
+++ tables.js
@@ -260,7 +260,7 @@ function lenteleAteitisSuPasirinkimais(pavadinimas, data, dataTotal, container) 
     }
 
     let table = `
-        <table class="table table-sm table-hover table-bordered">
+        <table class="table table-sm table-hover table-bordered lentele_${container}">
             <thead>
                 <tr>
                     <th style="background-color: ${dataTotal.color};" class="text-center align-middle small">
